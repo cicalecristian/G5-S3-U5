dc2e28893252d6fcb6b1303893f0f4e882e05a6d
cicalecristian <cicalecristian@gmail.com>
2026-04-24 15:09:39 +0200

    security token created

---
+++ src/main/java/cristiancicale/G5S3U5/security/TokenTools.java
@@ -0,0 +1,47 @@
+package cristiancicale.G5S3U5.security;
+
+import cristiancicale.G5S3U5.entities.Utente;
+import cristiancicale.G5S3U5.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class TokenTools {
+
+    private final String secret;
+
+    public TokenTools(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    public String generateToken(Utente utente) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .subject(String.valueOf(utente.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Problemi col token! Effettua di nuovo il login!");
+        }
+    }
+
+    public UUID extractIdFromToken(String token) {
+        return UUID.fromString(Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject());
+    }
+}
