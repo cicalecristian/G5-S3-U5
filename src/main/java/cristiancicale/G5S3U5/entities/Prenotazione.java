0c042167a30d55252089a8f6e6787064af20dda0
cicalecristian <cicalecristian@gmail.com>
2026-04-24 12:32:45 +0200

    entity Prenotazione created

--- src/main/java/cristiancicale/G5S3U5/entities/Prenotazione.java
+++ src/main/java/cristiancicale/G5S3U5/entities/Prenotazione.java
@@ -1,4 +1,37 @@
 package cristiancicale.G5S3U5.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "prenotazioni")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Prenotazione {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente utente;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "evento_id", nullable = false)
+    private Evento evento;
+
+    @Column(nullable = false)
+    private int posti_prenotati;
+
+    public Prenotazione(Utente utente, Evento evento, int posti_prenotati) {
+        this.utente = utente;
+        this.evento = evento;
+        this.posti_prenotati = posti_prenotati;
+    }
 }
